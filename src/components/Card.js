0fdcf525dcf16ad52815f96c1939e7faf84a3946
Thy Madona <thymadonakh@gmail.com>
2022-06-14 14:50:19 +0700

    travel journey react app practice

---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Card(props) {
+    return(
+        <div className="card">
+            <img className="card--img" src={props.imageUrl} />
+            <div className="card--info">
+                <div className="info--location">
+                    <h2>
+                        <span className="info--country">{props.location} </span>
+                        <span className="info-map"><a href={props.googleMapsUrl}>View on Google Map</a></span>
+                    </h2>
+                </div>
+                    <div className="card--title">
+                    <h1>{props.title}</h1>
+                </div>
+                <div className="card-date">
+                    <h2>
+                        <span className="date-start"> {props.startDate} - </span>
+                        <span className="date-end">{props.endDate}</span>
+                    </h2>
+                </div>
+                <div className="card-text">
+                    <p>
+                        {props.description}
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <nav>
+            <h1 className="nav--title">my travel journal</h1>
+        </nav>
+    );
+}
